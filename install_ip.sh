898d85fda09dc6de52cb0a0f257cd5e781ee7109
Arthur Fournier <44359684+space192@users.noreply.github.com>
2021-04-09 17:46:37 +0200

    Update install_ip.sh

--- install_ip.sh
+++ install_ip.sh
@@ -3,8 +3,6 @@ if [ "$EUID" -ne 0 ]
   then echo "Lancer le programme en administrateur"
   exit
 fi
-apt-get update
-apt-get upgrade -y
 apt-get install python3
 apt-get install pyhton3-pip
 apt-get install python3-pyqt5
